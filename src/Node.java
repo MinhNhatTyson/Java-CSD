d14784ff1402f9e33c823a9df8bbfcb8f05ebcc4
MinhNhatTyson <92989767+MinhNhatTyson@users.noreply.github.com>
2022-09-13 16:03:53 +0700

    Add files via upload

---
+++ src/Node.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Node;
+
+/**
+ *
+ * @author Admin
+ */
+public class Node {
+    protected int info;
+    protected Node next;
+    public Node() {
+    }
+
+    public Node(int x, Node y) {
+        this.info = x;
+        this.next = y;
+    }
+
+    public int getInfo() {
+        return info;
+    }
+
+    public void setInfo(int info) {
+        this.info = info;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+    
+   
+}
